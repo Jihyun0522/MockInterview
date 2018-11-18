cbe5aa6366dc5b5b153f9746677e22d6b2bb5b85
jihyun0522 <s2017s01@e-mirim.hs.kr>
2018-11-18 22:40:42 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/e_mirim/mockinterview/activity_menu.java
@@ -0,0 +1,63 @@
+package kr.hs.e_mirim.mockinterview;
+
+import android.app.TabActivity;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TabHost;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class activity_menu extends TabActivity {
+    int but = 0;
+    //private Button btn = (Button)findViewById(R.id.tip_btn1);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        final TabHost tabHost=getTabHost();
+        ArrayList<TabHost.TabSpec> tabSpecs= new ArrayList<TabHost.TabSpec>();
+        String[] texts={"면접", "Tip", "커뮤니티", "My"};
+        for(int i = 0; i < 4; i++){
+            tabSpecs.add(tabHost.newTabSpec("a" + (i+1)).setIndicator(texts[i]));
+        }
+
+        int j=0;
+        for(TabHost.TabSpec tabspec:tabSpecs){
+            tabspec.setContent(R.id.view1 + j);
+            tabHost.addTab(tabspec);
+            j++;
+        }
+
+        setTabColor(tabHost);
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+
+            @Override
+            public void onTabChanged(String arg0) {
+
+                setTabColor(tabHost);
+            }
+        });
+
+    }
+
+    public static void setTabColor(TabHost tabhost) {
+
+        for (int i = 0; i < tabhost.getTabWidget().getChildCount(); i++) {
+            tabhost.getTabWidget().getChildAt(i).setBackgroundResource(R.color.white/*R.drawable.header_blank*/); // unselected
+//            TextView tv = (TextView)tabhost.getTabWidget().getChildAt(tabhost.getCurrentTab());
+//            tv.setT
+        }
+        tabhost.getTabWidget().setCurrentTab(0);
+        tabhost.getTabWidget().getChildAt(tabhost.getCurrentTab())
+                .setBackgroundResource(R.color.colorMain/*R.drawable.tab_selected_new*/); // selected
+        // //have
+        // to
+        // change
+    }
+
+}
